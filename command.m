5fbe17c4b229b07c680a83492da09cdee859ba2a
Yoon <hyoon15@vols.utk.edu>
2019-11-20 13:14:09 -0500

    11/20 1:13pm, fixed summing up all the probabilities in "term"

--- command.m
+++ command.m
@@ -8,11 +8,12 @@ s_max = 10;
 p = 0.2;
 n = 5; % 20 in the paper
 r = 0;
-for i = 1:10
+for i = 1:s_max
     for j = 1:b
         g = j*(c_max-1)/c_max^2;
-        r = r + nchoosek(s_max,i)*g^i*(1-g)^(s_max-i)*...
-            nchoosek(c_max,j)*p^j*(1-p)^(c_max-j);
+        r = r + nchoosek(s_max-1,i-1)*g^(i-1)*(1-g)^(s_max-i)*...
+            nchoosek(c_max-1,j-1)*p^(j-1)*(1-p)^(c_max-j);
     end
 end
 [bval,bact] = sdp(b,x,y,r,z,s_max,c_max,p,n);
+bact
