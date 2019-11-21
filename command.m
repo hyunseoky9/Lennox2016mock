0c4b9e5a4ac724e9a5a2198dcc725a7c020017fc
Yoon <hyoon15@vols.utk.edu>
2019-11-20 20:37:41 -0500

    dynamic programming implemented

--- command.m
+++ command.m
@@ -6,17 +6,22 @@ z = 2;
 c_max = 10;
 s_max = 10;
 p = 0.2;
-n = 4; % 20 in the paper
+n = 1; % 20 in the paper
 r = 0;
+probm = zeros([s_max,c_max]);
+term = 0;
 for i = 1:s_max
     for j = 1:b
         g = j*(c_max-1)/c_max^2;
-        r = r + nchoosek(s_max-1,i-1)*g^(i-1)*(1-g)^(s_max-i)*...
+        term = nchoosek(s_max-1,i-1)*g^(i-1)*(1-g)^(s_max-i)*...
             nchoosek(c_max-1,j-1)*p^(j-1)*(1-p)^(c_max-j);
+        probm(i,j) = term;
+        r = r + term;
     end
 end
+probm = probm/r;
 tic
-[bval,bact] = sdp(b,x,y,r,z,s_max,c_max,p,n);
+[bval,bact] = sdp(b,x,y,r,z,s_max,c_max,probm,p,n);
 toc
 fprintf('n=%d\n',n);
 fprintf('bval=%.5f, bact=%d\n\n\n',bval,bact);
--- sdp.m
+++ sdp.m
@@ -1,10 +1,13 @@
-function [bestval,bestaction] = sdp(b,x,y,r,z,s_max,c_max,p,n) 
+function [bestval,bestaction] = sdp(b,x,y,r,z,s_max,c_max,probm,p,n) 
 %% function for stochastic dynamic programming
 % input:
 % b = budget; scalar
 % x = state space; 1x2 array 
 % y = action space (0=no buy, 1= buy); 1x2 array
 % r = sum of all the possible probabilities of buying land with s=i, c=j;
+% s_max = maximum conservation status value
+% c_max = maximum land cost value
+% probm = probability matrix where probm(i,j) is Pr of s=i,c=j
 % p = probability for binom dist for cost
 % output:
 % n = number of times game is played
@@ -15,15 +18,13 @@ val = zeros([1,length(y)]);
 if n > 1
     for action = 1:length(y)
         if y(action) == 0 %don't buy
-            val(action) = sdp(b,x,y,r,z,s_max,c_max,p,n-1); %%%%%
+            val(action) = sdp(b,x,y,r,z,s_max,c_max,probm,p,n-1); %%%%%
         elseif y(action) == 1 % buy
             val(action) = 0;
             for i = 1:s_max
                 for j = 1:b
                     g = j*(c_max-1)/c_max^2; %probability for conservation status determined by cost
-                    term = nchoosek(s_max-1,i-1)*g^(i-1)*(1-g)^(s_max-i)*...
-                        nchoosek(c_max-1,j-1)*p^(j-1)*(1-p)^(c_max-j)/r;
-                    val(action) = val(action) + term*(i^z + sdp(b,x+i^z,y,r,z,s_max,c_max,p,n-1));
+                    val(action) = val(action) + probm(i,j)*(i^z + sdp(b,x+i^z,y,r,z,s_max,c_max,probm,p,n-1));
                 end
             end
         else % borrow (for later)
@@ -35,11 +36,9 @@ else %last recursion
             val(action) = x;
         elseif y(action) == 1
             for i = 1:s_max
-                for j = 1:c_max
+                for j = 1:b
                     g = j*(c_max-1)/c_max^2; %probability for conservation status determined by cost
-                    term = nchoosek(s_max-1,i-1)*g^(i-1)*(1-g)^(c_max-i)*...
-                        nchoosek(c_max-1,j-1)*p^(j-1)*(1-p)^(s_max-j)/r;
-                    val(action) = val(action) + term*(i^z);
+                    val(action) = val(action) + probm(i,j)*(i^z);
                 end
             end
             val(action) = val(action) + x;
