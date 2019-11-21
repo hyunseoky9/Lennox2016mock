30b75e3de33306d21bb5970409b8d94031de0275
Hyun Seok Yoon <hy324@cornell.edu>
2019-11-21 17:29:36 -0500

    entering borrowing

--- command.m
+++ command.m
@@ -1,10 +1,13 @@
 %%command script for running sdp function
+%% create optm tensor that stores all the information for best action given state variable
+%% at certain timestep (size = c_max*s_max*n)
 b = 5;
 z = 1;
 c_max = 10;
 s_max = 10;
 p = 0.2;
-n = 1; % 240 in the paper
+n = 240; % 240 in the paper
+
 probm = zeros([s_max,c_max]);
 term = 0;
 for i = 1:s_max
@@ -19,12 +22,12 @@ optm = zeros([s_max,c_max,n]); %optimal action table given state vector and time
 bestvalarray = zeros([s_max,c_max]);
 bestvalarray_new = zeros([s_max,c_max]);
 tic
-for k = n:1
+for k = n:-1:1
 	ss = sum(probm.*bestvalarray,'all');
 	for i = 1:s_max
 		for j = 1:c_max
 			if j > b
-				[bestvalarray_new(i,j),optm(i,j,k)] = findbest(i,j,d,[0],z,ss,k);
+				[bestvalarray_new(i,j),optm(i,j,k)] = findbest(i,j,d,[0,2],z,ss,k);
 			else
 				[bestvalarray_new(i,j),optm(i,j,k)] = findbest(i,j,d,[0,1],z,ss,k);
 			end
@@ -33,3 +36,15 @@ for k = n:1
 	bestvalarray = bestvalarray_new;
 end
 toc
+%disp(bestvalarray);
+%disp(optm);
+%write out optm into csv file
+fileID = fopen('optimal_aciton.csv','w');
+for k = 1:n
+	for j=1:c_max
+
+		fprintf(fileID,'a');
+	end
+end
+
+fclose(fileID);
---
+++ findbest.m
@@ -0,0 +1,31 @@
+function [bestval,bestaction] = findbest(s,c,d,y,z,ss,n)
+%% update optimal action matrix and best value for the given state and time.
+% input:
+% c = cost; scalar (state variable 2)
+% s = conservation status; scalar (state variable 3)
+% d = debt; scalar (state variable 4)
+% y = action space; array (either {0},{0,1}, or {0,1,2}if there's borrowing)
+% s_max = maximum conservation status value
+% z = exponent for calculating conservation value from status.
+% ss = sum of element multiplication matrix of probm and besetvalarray matrix.
+% n = number of backwards recursions to do.
+% output:
+% bestval = best value given best action
+% bestaction = best action given state 
+val = zeros([1,length(y)]);
+bestaction = 0;
+bestval = 0;
+for action = 1:length(y)
+	if y(action) == 0 % don't buy/can't buy
+		val(action) = 0 + ss;
+    elseif y(action) == 1 % buy
+		val(action) = s^z + ss;
+	else % borrow and buy
+		foo = 1;
+		%do it later
+	end
+end
+[bestval,bestaction] = max(val);
+bestaction = y(bestaction);
+
+
