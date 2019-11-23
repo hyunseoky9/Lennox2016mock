0213cc7f2b64b6e79bb9add75518392f45690eef
Hyun Seok Yoon <hyoon15@vols.utk.edu>
2019-11-22 19:30:43 -0500

    simulation all coded

--- simulation.m
+++ simulation.m
@@ -1,28 +1,37 @@
-b = 5; % budget
+b_default = 5; % budget
 z = 1; % conservation value parameter (conservation value) = (conservation status)^z
 c_max = 10; % maximum cost of a parcel allowed (in $100,000 in the paper)
 s_max = 10; % maximum cons. status of a parcel allowed
 d_max = b; % maximum debt allowed to accrue. set to 0 if no borrowing allowed (fixed)
 p = 0.2; % probability parameter in the binomial distribution that chooses parcel cost
 n = 10; % 240 in the paper
+modval = [0 0] % model value calculator at the end
+rep = 1000; %  simulation rep num
 for mo = 1:2 % run sim 2 times with dif model
-	if mo == 1 % fixed
-		d_max = 0;
-	else % borrow
-		d_max = b;
-	end
-end
+		if mo == 1 % fixed
+			d_max = 0;
+		else % borrow
+			d_max = b;
+		end
 
-optm = opt_act(b,z,c_max,s_max,d_max,p,n);
-
-rep = 1000; % repetition simulation
-val_f = 0; % consrevation value counter for fixed
-val_b = 0; % counter for borrow
-for i = 1:rep
-	for t = 1:n
-		c = binornd(c_max,p);
-		g = c*(c_max-1)/c_max^2;
-		s = binornd(s_max,g);
-		val_f = optm()
+	optm = opt_act(b_default,z,c_max,s_max,d_max,p,n);
+	modval_accum = 0;
+	for i = 1:rep
+		modvalpt = 0; % model value calculator per time
+		d = 0;
+		for t = 1:n
+			c = binornd(c_max,p);
+			g = c*(c_max-1)/c_max^2;
+			s = binornd(s_max,g);
+			b = b_default;
+			repay = ceil(d/3);
+			d = d - repay;
+			if optm(s,c,d,t) == 1 % buy
+				modvalpt = modvalpt + s^z
+				d = c - (b - repay);
+			end
+		end
+		modval_accum = modval_accum + modvalpt;
 	end
+	modval(mo) = modval_accum/rep
 end
