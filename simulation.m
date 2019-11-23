4fd914621fe5fc23d28db38450c01d2d0631edb9
Hyun Seok Yoon <hyoon15@vols.utk.edu>
2019-11-22 19:55:05 -0500

    more commenting on simulation

--- simulation.m
+++ simulation.m
@@ -1,3 +1,8 @@
+% simulation script. Uses function opt_act
+% output of the script is the mean conservation value for borrowing and fixed model
+% set parameter and RUN!
+
+%%%%%%% parameters to set%%%%%%%%%%
 b_default = 5; % budget
 z = 1; % conservation value parameter (conservation value) = (conservation status)^z
 c_max = 10; % maximum cost of a parcel allowed (in $100,000 in the paper)
@@ -5,8 +10,10 @@ s_max = 10; % maximum cons. status of a parcel allowed
 d_max = b; % maximum debt allowed to accrue. set to 0 if no borrowing allowed (fixed)
 p = 0.2; % probability parameter in the binomial distribution that chooses parcel cost
 n = 10; % 240 in the paper
-modval = [0 0]; % model value calculator at the end
 rep = 1000; %  simulation rep num
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+modval = [0 0]; % model value calculator at the end
 for mo = 1:2 % run sim 2 times with dif model
 	if mo == 1 % fixed
 		d_max = 0;
@@ -15,26 +22,28 @@ for mo = 1:2 % run sim 2 times with dif model
 	end
 	optm = opt_act(b_default,z,c_max,s_max,d_max,p,n); % get best action space
 	modval_accum = 0;
-	for i = 1:rep
+	for i = 1:rep % simulation start
 		modvalpt = 0; % model value calculator per time
-		d = 0;
+		d = 0; % initial debt 0
 		for t = 1:n
-			c = binornd(c_max-1,p) + 1;
-			g = c*(c_max-1)/c_max^2;
-			s = binornd(s_max-1,g) + 1;
-			b = b_default;
-			repay = ceil(d/3);
-			d = d - repay;
+			c = binornd(c_max-1,p) + 1; % c instance
+			g = c*(c_max-1)/c_max^2; % binom prob parameter for s (dep. on c)
+			s = binornd(s_max-1,g) + 1; % s instance
+			b = b_default; % budget
+			repay = ceil(d/3); % payment of the debt in the beginning of time step
+			d = d - repay; % debt reduced
 			if optm(s,c,d+1,t) == 1 % buy
-				modvalpt = modvalpt + s^z;
-				if c > (b - repay);
+				modvalpt = modvalpt + s^z; % add conservation value by buying
+				if c > (b - repay) % debt increased if borrowed
 					d = c - (b - repay);
 				end
 			end
 		end
 		modval_accum = modval_accum + modvalpt;
 	end
-	modval(mo) = modval_accum/rep;
+	modval(mo) = modval_accum/rep; % mean overall value throughout time recorded per model
 end
 
-disp(modval);
+fprintf("\n\nmean conservation value by model\n");
+fprintf("fixed = %.3f\n",modval(1));
+fprintf("borrow = %.3f\n",modval(2));
