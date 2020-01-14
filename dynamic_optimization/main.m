3cefeecfebd6102cdbee568066648f0437581da2
Yoon <hyoon15@vols.utk.edu>
2020-01-13 22:06:57 -0500

    1/13/2020

--- dynamic_optimization/main.m
+++ dynamic_optimization/main.m
@@ -78,3 +78,71 @@ fprintf("cor(fr,f)= %.2f\n",cor(1,2))
 cor = corrcoef(fb,f);
 fprintf("cor(fb,f)= %.2f\n",cor(1,2))
 
+%% simulation
+burnin = 200; % burn in first few values of net return as they have not converged yet
+f = f(burnin:end);
+ff = ff(burnin:end);
+fb = fb(burnin:end);
+fr = fr(burnin:end);
+
+simtime = 1; % number of buying opportunities
+for i = 1:simtime
+  f = f(i:end);
+  ff = ff(i:end);
+  fb = fb(i:end);
+  fr = fr(i:end);
+
+  bfn = 1; % benefit fn scheme. 1=constant, 2=normal var correlated to e_fj, 3=non-linear fn of 2
+
+  e_rj = normrnd(0,0); % indiv dev var
+  efmu = 0;
+  efsig = 1;
+  if bfn == 1
+    e_fj = normrnd(efmu,efsig);
+    b = 10;
+  else
+    mu = [efmu 10];
+    sigma = [efsig 1.5; 1.5 1];
+    R = mvnrnd(mu,sigma,1);
+    e_fj = R(1); % indiv forest var
+    b = R(2); % for benefit
+  end
+  f_fj = ff _ e_fj;
+  f_fj(f_fj<0) = 0;
+  f_rj = fr _ e_rj;
+  f_rj(f_rj<0) = 0;
+
+  rho = 0.1; % economic discount rate
+  del = 0.01; % ecological discount rate
+
+  % getting clearing time t_j
+  t_j = 1;
+  edisc = repelem(1+rho,length(f)-burnin+1).^(1:length(f)-burnin+1); %economic discount rate.
+  tjoptim = sum((f_rj(t_j:end)-f_fj(t_j:end))./edisc(1:(end-t_j+1)));
+  while tjval <= 0
+    t_j = t_j + 1;
+    tjoptim = sum((f_rj(t_j:end)-f_fj(t_j:end))./edisc(1:(end-t_j+1)));
+  end
+
+  % cost
+  if t_j == 1
+    c = sum(f_rj(t_j:end)./edisc(t_j:end));
+  else
+    c = sum(f_fj(1:(t_j-1))./edisc(1:(t_j-1))) + sum(f_rj(t_j:end)./edisc(t_j:end));
+  end
+
+  %benefit
+  if bfn == 3
+    b = b^(1/2);
+  end
+  ecodisc = repelem(1+del,length(f)-burnin+1).^(1:length(f)-burnin+1); %ecological discount rate
+  B = sum(b./ecodisc(t_j:end)); % conservation value (incl' discount and threat component)
+
+  % evaluating and buying process
+  al = 1;
+  be = 1;
+  fund = ??
+  cval = (B/c)^al*fund^be; % conservation value
+
+end
+
